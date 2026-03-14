043591c5869adc230ce777d55f786b733a93d96a
andreysk0304 <andreysk0304@yandex.ru>
2026-03-14 14:53:56 +0300

    delete yandex mail tool

--- model_tools.py
+++ model_tools.py
@@ -82,7 +82,6 @@ def _discover_tools():
         "tools.skills_tool",
         "tools.skill_manager_tool",
         "tools.browser_tool",
-        "tools.yandex_mail_tool",
         "tools.cronjob_tools",
         "tools.rl_training_tool",
         "tools.tts_tool",
--- toolsets.py
+++ toolsets.py
@@ -46,8 +46,6 @@ _HERMES_CORE_TOOLS = [
     "browser_type", "browser_scroll", "browser_back",
     "browser_press", "browser_close", "browser_get_images",
     "browser_vision",
-    # Email UI (gated by env)
-    "yandex_mail_fetch",
     # Text-to-speech
     "text_to_speech",
     # Planning & memory
@@ -121,8 +119,7 @@ TOOLSETS = {
             "browser_navigate", "browser_snapshot", "browser_click",
             "browser_type", "browser_scroll", "browser_back",
             "browser_press", "browser_close", "browser_get_images",
-            "browser_vision", "web_search",
-            "yandex_mail_fetch",
+            "browser_vision", "web_search"
         ],
         "includes": []
     },
